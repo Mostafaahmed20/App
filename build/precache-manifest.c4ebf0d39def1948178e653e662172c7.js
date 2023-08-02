a0fba80ef48127701c8b8f5fff085b6d63ff639a
mostafa ahmed <42159268+Mostafaahmed20@users.noreply.github.com>
2023-08-02 22:33:46 +0300

    app

---
+++ build/precache-manifest.c4ebf0d39def1948178e653e662172c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d728067a1fac98b7f072",
+    "url": "/static/css/main.6a64724c.chunk.css"
+  },
+  {
+    "revision": "d728067a1fac98b7f072",
+    "url": "/static/js/main.7f8ffb82.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2758281c1db03ce7ac4c",
+    "url": "/static/css/2.e40c88b9.chunk.css"
+  },
+  {
+    "revision": "2758281c1db03ce7ac4c",
+    "url": "/static/js/2.bab181b4.chunk.js"
+  },
+  {
+    "revision": "fa29580f19f015dbcb911da76ddc83e9",
+    "url": "/static/media/daniel-olah-2lMK4dgqwFM-unsplash.fa29580f.jpg"
+  },
+  {
+    "revision": "668baec712d686ffaa4153d56eecdbe5",
+    "url": "/index.html"
+  }
+];
